9d1dacb76f2438ad62c30f2c1ac82ec9b77dcd9c
root <root@ASUS>
2023-07-08 09:46:05 +0530

    update

--- exp1/exp1.py
+++ exp1/exp1.py
@@ -8,27 +8,27 @@ from nest.topology import *
 #                                                  #
 #                                                  #
 #                                                  #
-#  N0------------------n2---------------------- N2 #
+#  N0------------------n2---------------------- N3 #
 #         <------ 5mbit, 2ms ------>               #
 #                                                  #
 ####################################################
 
 # Created 3 Nodes
 n0 = Node("n0")
-n2 = Node("n2")
+n3 = Node("n3")
 n2 = Switch("n2")
 
 # Connect the nodes to switch n2
 (etn0, etn2a) = connect(n0, n2)
-(etn2, etn2b) = connect(n2, n2)
+(etn3, etn2b) = connect(n3, n2)
 
 # Assigning IP Address to the Nodes
 etn0.set_address("10.0.0.1/24")
-etn2.set_address("10.0.0.2/24")
+etn3.set_address("10.0.0.2/24")
 
 # Set the bandwidth and the delay between the nodes
 etn0.set_attributes("5mbit", "2ms")
-etn2.set_attributes("5mbit", "2ms")
+etn3.set_attributes("5mbit", "2ms")
 
 # Ping from Node1 to Node3
-n0.ping(etn2.address)
+n0.ping(etn3.address)
