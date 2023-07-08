7f08c9473b2fbacb21290712d108821b53f14b42
root <root@ASUS>
2023-07-08 09:59:02 +0530

    update

--- exp1/exp1.py
+++ exp1/exp1.py
@@ -1,4 +1,5 @@
 from nest.topology import *
+from nest.experiment import *
 
 # Simulate a three nodes point-to-point network with duplex links between them. Set the queue
 # size vary the bandwidth and find the number of packets dropped.
@@ -8,27 +9,43 @@ from nest.topology import *
 #                                                  #
 #                                                  #
 #                                                  #
-#  N0------------------n2---------------------- N3 #
+#  N1------------------n2---------------------- N3 #
 #         <------ 5mbit, 2ms ------>               #
 #                                                  #
 ####################################################
 
 # Created 3 Nodes
-n0 = Node("n0")
+n1 = Node("n1")
 n3 = Node("n3")
 n2 = Switch("n2")
 
 # Connect the nodes to switch n2
-(etn0, etn2a) = connect(n0, n2)
+(etn1, etn2a) = connect(n1, n2)
 (etn3, etn2b) = connect(n3, n2)
 
 # Assigning IP Address to the Nodes
-etn0.set_address("10.0.0.1/24")
+etn1.set_address("10.0.0.1/24")
 etn3.set_address("10.0.0.2/24")
 
-# Set the bandwidth and the delay between the nodes
-etn0.set_attributes("5mbit", "2ms")
-etn3.set_attributes("5mbit", "2ms")
+# adding a choke Queue Discipline
+qdisc = "choke"
+choke_parameters = {
+    "limit": "100",  # set the queue capacity to 100 packets
+    "min": "5",  # set the minimum threshold to 5 packets
+    "max": "15",  # set the maximum threshold to 15 packets
+}
 
-# Ping from Node1 to Node3
-n0.ping(etn3.address)
+# Set the bandwidth and the delay between the nodes
+etn1.set_attributes("5mbit", "2ms", qdisc, **choke_parameters)
+etn3.set_attributes("5mbit", "2ms", qdisc, **choke_parameters)
+
+# Created an new experiment
+exp = Experiment("three-node-point-to-point")
+
+# creating a new Flow from `n1` to `n3` .
+flow1 = Flow(n1, n3, etn3.get_address(), 0, 50, 1)
+
+# Use `flow1` as a TCP flow .
+exp.add_udp_flow(flow1, target_bandwidth="5mbit")
+
+exp.run()
