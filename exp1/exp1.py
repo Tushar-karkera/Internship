d1c43c8b2f97b425b8c50461c35e09d41da29883
root <root@ASUS>
2023-07-09 21:06:33 +0530

    updated

--- exp1/exp1.py
+++ exp1/exp1.py
@@ -1,6 +1,5 @@
 from nest.topology import *
 from nest.experiment import *
-from nest.experiment.tools import Iperf3
 
 # Simulate a three nodes point-to-point network with duplex links between them. Set the queue
 # size vary the bandwidth and find the number of packets dropped.
@@ -39,30 +38,27 @@ h2.add_route("DEFAULT", eth2)
 qdisc = "pfifo"
 pfifo_parameters = {
     "limit": "20",  # set the queue capacity to 20 packets
-    "flows": "1",  # set the number of flows to 2
-    "target": "2ms",  # set the target queue delay to 2ms (default is 5ms)
-    "interval": "24ms",  # set the interval value to 24ms (default is 100ms)
 }
 
 # Setting the bandwidth and the delay between the nodes
 # also setting the queue size previously configured
-eth1.set_attributes("50mbit", "2ms")
-eth2.set_attributes("50mbit", "2ms")
+eth1.set_attributes("5mbit", "2ms")
+eth2.set_attributes("5mbit", "2ms")
 
-etr1a.set_attributes("50mbit", "2ms")
-etr1b.set_attributes("50mbit", "2ms", qdisc, **pfifo_parameters)
+etr1a.set_attributes("5mbit", "2ms")
+etr1b.set_attributes("5mbit", "2ms", qdisc, **pfifo_parameters)
 
 # Creating an new experiment
 exp = Experiment("three-node-point-to-point")
 
-# creating a new Flow from `h1` to `h2` for 20 seconds.
+# creating a new Flow from `h1` to `h2` for 10 seconds.
 flow1 = Flow(h1, h2, eth2.get_address(), 0, 10, 1)
 
 # Use `flow1` as a UDP flow with target bandwidth of 5mbit.
 exp.add_udp_flow(flow1, target_bandwidth="5mbit")
 
 # Enable statistics on the etr1a interface of the router
-# exp.require_qdisc_stats(etr1b)
+exp.require_qdisc_stats(etr1b)
 
 # Run the experiment
 exp.run()
