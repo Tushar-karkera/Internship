5a11f2124d5f185af4b4760bffd8120ed8802173
root <root@ASUS>
2023-07-10 10:24:54 +0530

    update

--- exp1/exp1.py
+++ exp1/exp1.py
@@ -5,7 +5,7 @@ from nest.experiment import *
 # size vary the bandwidth and find the number of packets dropped.
 
 ####################################################
-#                          Network Topology        #
+#               Network Topology                   #
 #                                                  #
 #                                                  #
 #                                                  #
@@ -14,34 +14,36 @@ from nest.experiment import *
 #                                                  #
 ####################################################
 
-# Created 2 Nodes and a router
+# Created 2 Nodes and a router.
 h1 = Node("h1")
 h2 = Node("h2")
 r1 = Router("r1")
 
-# Connect the nodes to router r1
+# Connect the nodes to router r1.
 (eth1, etr1a) = connect(h1, r1)
 (eth2, etr1b) = connect(h2, r1)
 
-# Assigning IP Address to the node and router interfaces
+# Assigning IP Address to the node and router interfaces.
 eth1.set_address("10.0.0.1/24")
 etr1a.set_address("10.0.0.2/24")
 
 etr1b.set_address("10.0.1.2/24")
 eth2.set_address("10.0.1.2/24")
 
-# Add default route for the gateway
+# Add a default route for the router as its gateway.
 h1.add_route("DEFAULT", eth1)
 h2.add_route("DEFAULT", eth2)
 
-# configuring the queue size
+# configuring the queue size.
+# applying Packet Limited queue in this scenario.
 qdisc = "pfifo"
 pfifo_parameters = {
     "limit": "20",  # set the queue capacity to 20 packets
 }
 
-# Setting the bandwidth and the delay between the nodes
-# also setting the queue size previously configured
+# Setting the bandwidth and the delay between the nodes as 5mbit
+# and 2ms respectively.
+# also applying the queue discipline previously configured on the etr1b interface.
 eth1.set_attributes("5mbit", "2ms")
 eth2.set_attributes("5mbit", "2ms")
 
@@ -54,11 +56,13 @@ exp = Experiment("three-node-point-to-point")
 # creating a new Flow from `h1` to `h2` for 10 seconds.
 flow1 = Flow(h1, h2, eth2.get_address(), 0, 10, 1)
 
-# Use `flow1` as a UDP flow with target bandwidth of 5mbit.
-exp.add_udp_flow(flow1, target_bandwidth="5mbit")
+# Use `flow1` as a UDP flow with target bandwidth of 50mbit.
+# Using higher bandwidth on the UDP flow to bottleneck the network and lead to packet drops.
+exp.add_udp_flow(flow1, target_bandwidth="50mbit")
 
-# Enable statistics on the etr1a interface of the router
-exp.require_qdisc_stats(etr1b)
+# Enable statistics on the etr1b interface of the router.
+# Temporarily disabled queue statistics due to unresolved errors.
+# exp.require_qdisc_stats(etr1b)
 
 # Run the experiment
 exp.run()
